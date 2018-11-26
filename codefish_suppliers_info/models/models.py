0c6135ba5653080075ce46ea2dfc77b8bab3afa3
CodeFish <msetiha@gmail.com>
2018-11-26 12:38:18 +0200

    New Module

---
+++ codefish_suppliers_info/models/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProductSupplierinfo(models.Model):
+    _inherit = 'product.supplierinfo'
+
+    standard_price = fields.Float('Cost', related='product_id.standard_price')
+    qty_available = fields.Float('Quantity On Hand', related='product_id.qty_available')
