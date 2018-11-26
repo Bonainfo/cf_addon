bf47416b1822075aee4855ca75727250b6ce066b
CodeFish <msetiha@gmail.com>
2018-11-26 13:31:16 +0200

    Update Module

--- codefish_suppliers_info/models/models.py
+++ codefish_suppliers_info/models/models.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
-    standard_price = fields.Float('Cost', related='product_id.standard_price')
+    standard_price = fields.Float('Cost', related='product_tmpl_id.standard_price')
     qty_available = fields.Float('Quantity On Hand', related='product_tmpl_id.qty_available')
