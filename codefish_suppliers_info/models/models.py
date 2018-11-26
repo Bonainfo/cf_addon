fa8ce69839dad2328c413261804a94b02cbf9b4b
CodeFish <msetiha@gmail.com>
2018-11-26 14:26:39 +0200

    Update Module

--- codefish_suppliers_info/models/models.py
+++ codefish_suppliers_info/models/models.py
@@ -7,3 +7,10 @@ class ProductSupplierinfo(models.Model):
 
     standard_price = fields.Float('Cost', related='product_tmpl_id.standard_price')
     qty_available = fields.Float('Quantity On Hand', related='product_tmpl_id.qty_available')
+    total = fields.Float(compute='_value_pc', string='Total')
+
+    @api.multi
+    @api.depends('standard_price', 'qty_available')
+    def _value_pc(self):
+        for record in self:
+            record.total = record.standard_price + record.qty_available
