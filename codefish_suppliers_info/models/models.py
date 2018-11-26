ecb95837a3d6495b44a4419e50c70a5a3120515c
CodeFish <msetiha@gmail.com>
2018-11-26 13:21:13 +0200

    Update Module

--- codefish_suppliers_info/models/models.py
+++ codefish_suppliers_info/models/models.py
@@ -6,4 +6,4 @@ class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
     standard_price = fields.Float('Cost', related='product_id.standard_price')
-    qty_available = fields.Float('Quantity On Hand', related='product_id.qty_available')
+    qty_available = fields.Float('Quantity On Hand', related='product_tmpl_id.qty_available')
